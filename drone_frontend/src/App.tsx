3a4ee25429a3cfda641d3abc5955f751dfa1621d
Christopher Langlands <chris.paul@droneshield.com>
2024-09-05 11:22:44 +1000

    remove dysfunctional try catch

--- drone_frontend/src/App.tsx
+++ drone_frontend/src/App.tsx
@@ -6,18 +6,15 @@ const App = () => {
 	useEffect(() => {
 		const websocket = new WebSocket('ws://localhost:8080/')
 
-		try {
-			websocket.onopen = () => {
-				console.log('Web Socket connected.')
-			}
-
-			websocket.onmessage = (event) => {
-				const data = JSON.parse(event.data)
-				console.log(data)
-			}
-		} catch (e) {
-			console.log('Web socket connection error: ', e)
+		websocket.onopen = () => {
+			console.log('Web Socket connected.')
 		}
+
+		websocket.onmessage = (event) => {
+			const data = JSON.parse(event.data)
+			console.log(data)
+		}
+
 	}, [])
 
 	return <Map />
